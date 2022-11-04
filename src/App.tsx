c2f50841d2cc0d863a2fbab51ea24bdadbc21bb1
Guilherme <gui_g88@hotmail.com>
2022-11-04 13:05:53 -0300

    use script

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,24 @@
+import { useScript } from './hooks/useScript';
+
+declare const TEST_SCRIPT: {
+  start: () => string;
+};
+
 export default function App() {
+  const status = useScript(
+    'https://pm28k14qlj.codesandbox.io/test-external-script.js'
+  );
+
   return (
-    <p className='font-sans mx-0 text-indigo-500 font-bold text-center'>App</p>
+    <div>
+      <div>
+        Script status: <b>{status}</b>
+      </div>
+      {status === 'ready' && (
+        <div className='font-sans mx-0 text-indigo-500 font-bold'>
+          Script function call response: <b>{TEST_SCRIPT.start()}</b>
+        </div>
+      )}
+    </div>
   );
 }
